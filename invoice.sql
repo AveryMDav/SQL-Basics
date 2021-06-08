21b2df924e2e70974cfaacc27beee9a3a783537a
Avery.Davenport <averymdavenport@gmail.com>
2021-06-08 15:58:46 -0600

    fifth-commit

---
+++ invoice.sql
@@ -0,0 +1,29 @@
+-- problem 1
+-- select count(billing_country) from invoice where billing_country = 'USA';
+
+-- problem 2
+-- select max(total) from invoice;
+
+-- problem 3
+-- select min(total) from invoice;
+
+-- problem 4
+-- select * from invoice where total > 5;
+
+-- problem 5
+-- select count(total) from invoice where total < 5;
+
+-- problem 6
+-- select count(billing_state) from invoice where billing_state in ('CA', 'TX', 'AZ');
+
+-- problem 7
+-- select avg(total) from invoice;
+
+-- problem 8
+-- select sum(total) from invoice;
+
+-- problem 9
+-- update invoice set total = 24 where invoice_id = 5;
+
+-- problem 10
+-- delete from invoice where invoice_id = 1;
